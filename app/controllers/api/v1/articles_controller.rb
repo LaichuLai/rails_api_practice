051571cf97726bfe3dbde1dae30d65f0a5f571ec
Gary Lai <cozzichilli@gmail.com>
2022-03-27 18:02:49 +0800

    feat: initialize project and ues mysql2 as DB

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,51 @@
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+
+      def index
+        articles = Article.order('created_at DESC');
+        render json: {status: 'SUCCESS', message: 'Loaded articles', data:articles}, status: :ok
+      end
+
+      def show
+        article = Article.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded article', data:article}, status: :ok
+      end
+
+      def create
+        article = Article.new(article_params)
+
+        if article.save
+          render json: {status: 'SUCCESS', message: 'Loaded articles', data:article}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Article not saved', data:article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        article = Article.find(params[:id])
+        article.destroy
+        render json: {status: 'SUCCESS', message: 'Article deleted', data:article}, status: :ok
+      end
+
+      def update
+        article = Article.find(params[:id])
+        if article.update_attributes(article_params)
+          render json: {status: 'SUCCESS', message: 'Updated articles', data:article}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Article not saved', data:article.errors}, status: :unprocessable_entity
+        end
+      end
+
+
+
+      private
+
+      def article_params
+        params.permit(:title, :body)
+      end
+
+
+    end
+  end
+end
